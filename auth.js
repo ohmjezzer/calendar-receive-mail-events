5468f9a2259c66831727c68a6816a038d568c5d1
ohmjezz <jessadanaka12@gmil.com>
2025-08-31 11:51:06 +0700

    first commit

---
+++ auth.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const { google } = require('googleapis');
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+const TOKEN_PATH = path.join(__dirname, 'token.json');
+function authorize() {
+  const credentials = JSON.parse(fs.readFileSync('credentials.json'));
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+  if (fs.existsSync(TOKEN_PATH)) {
+    const token = fs.readFileSync(TOKEN_PATH);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    return oAuth2Client;
+  } else {
+    return getAccessToken(oAuth2Client);
+  }
+}
+function getAccessToken(oAuth2Client) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  // User should visit the above URL and paste the code here:
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
+      console.log('Token stored to', TOKEN_PATH);
+    });
+  });
+}
+module.exports = authorize;
